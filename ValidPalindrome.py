1c27fc4597eb7aee9d3a4912c98b7c3ae63c9a80
abimbola-ai <aojikutu20@aiip.alueducation.com>
2023-11-17 13:11:53 -0500

    Added new challenges

---
+++ ValidPalindrome.py
@@ -0,0 +1,42 @@
+# 125. Valid Palindrome
+
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+# Example 1:
+
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+# Example 2:
+
+# Input: s = "race a car"
+# Output: false
+# Explanation: "raceacar" is not a palindrome.
+# Example 3:
+
+# Input: s = " "
+# Output: true
+# Explanation: s is an empty string "" after removing non-alphanumeric characters.
+# Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 2 * 105
+# s consists only of printable ASCII characters.
+
+import re
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # _s = s.replace(" ","").lower()
+        s_clean = (re.sub(r'[^A-Za-z0-9]+', '', s)).lower()
+        print(s_clean)
+        if s_clean == s_clean[::-1]:
+            return True
+        else:
+            False
+        
